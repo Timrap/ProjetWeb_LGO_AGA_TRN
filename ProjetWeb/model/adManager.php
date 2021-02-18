c214390877c932b2632cb1fc479b43d6aa87a83d
TimotheeRapin <timothee.rapin@cpnv.ch>
2021-02-18 11:48:00 +0100

    Enregistrement d'une annonce dans ad.json v1.0

---
+++ ProjetWeb/model/adManager.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @param $adTitle
+ * @param $adContent
+ * @return bool
+ */
+function registerNewAd($adTitle, $adDescription, $adPrice)
+{
+    $result = false;
+
+//  Create an array to add in JSON file
+
+    $data2add = array('title' => $adTitle, 'content' => $adDescription, 'price' => $adPrice, 'adType' =>'1');
+
+    $file = "model/data/ad.json";
+
+//open or read json data
+    $data_results = file_get_contents($file);
+    $tempArray = json_decode($data_results);
+
+//append additional json to json file
+    $tempArray[] = $data2add ;
+    $jsonData = json_encode($tempArray);
+
+    file_put_contents($file, $jsonData);
+
+//    json_encode($data);
+    return true;
+}
---
+++ ProjetWeb/view/adMenu.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @file      login.php
+ * @brief     This view is designed to display the login form
+ * @author    Created by Pascal.BENZONANA
+ * @author    Updated by Nicolas.GLASSEY
+ * @version   13-APR-2020
+ */
+
+$title = 'annoncesfaciles . Login/Logout';
+
+ob_start();
+?>
+<?php //if ($loginErrorMessage != null) : ?>
+<?php if (isset($loginErrorMessage)) : ?>
+    <h5><span style="color:red"><?= $loginErrorMessage; ?></span></h5>
+<?php endif ?>
+
+    <!-- Title Page -->
+    <div id="page" class="container" >
+
+
+        <!-- content page -->
+        <section class="bgwhite p-t-66 p-b-60">
+            <div class="container">
+                <div class="row">
+
+                <div class="col-md-12 p-b-30" id="formulaire1">
+
+                        <h2 class="m-text26 p-b-36 p-t-15">
+                            Menu annonces
+                        </h2>
+<center>
+                    <a href="index.php?action=home"><input type="submit" value="Visionner mes annonces" class="flex-c-m size10 bg4 bo-rad-23 hov1 m-text3 trans-0-4" id="boutonmenuad"</a></a>
+                    <br><br>
+                    <a href="index.php?action=createAd"><input type="submit" value="Insérer une annonce" class="flex-c-m size10 bg4 bo-rad-23 hov1 m-text3 trans-0-4" id="boutonmenuad" </a></a>
+</center>
+
+
+
+
+
+
+
+
+                </div>
+            </div>
+    </div>
+    </section>
+    </div>
+<?php
+$content = ob_get_clean();
+require 'gabarit.php';
+?>
