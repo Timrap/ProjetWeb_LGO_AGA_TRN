4ec82456cb7f40fc972f604f0379434481802ca3
LucaGattocpnv <61775734+LucaGattocpnv@users.noreply.github.com>
2021-03-01 11:25:15 +0100

    ajout liste déroulante

--- ProjetWeb/view/createad.php
+++ ProjetWeb/view/createad.php
@@ -26,27 +26,51 @@ ob_start();
                 <div class="row">
                     <div class="col-md-12 p-b-30" id="formulaire1">
                         <form class="leave-comment" action="index.php?action=validationAd" method="post" >
+
                             <h2 class="m-text26 p-b-36 p-t-15">
                                 Insérer une annonce
                             </h2>
+
+                            <br>
+
                             <h4>
                                 Informations du produit
                             </h4>
+
+                            <br>
+
+                            <!-- liste deroulante -->
+                            <h4>
+                                <select name="menu_destination">
+                                    <option>Choisir une catégorie</option>
+                                    <option>Véhicule Motorisé</option>
+                                    <option>Appareil éléctronique</option>
+                                    <option>Mobilier</option>
+                                    <option>Bijou</option>
+                                    <option>Immobilier</option>
+                                </select>
+                            </h4>
+
+                            <!-- champs titre -->
                             <div class="bo4 of-hidden size15 m-b-20" >
                           <br> <input class="sizefull s-text7 p-l-22 p-r-22" id="description" type="text" name="title" placeholder="Titre du produit" required>
                             </div>
 <br>
+                            <!-- champs description -->
                             <div class="bo4 of-hidden size15 m-b-20">
                               <textarea id="description" name="description" cols="50" rows="10" placeholder="Description" required></textarea>
                             </div class="md-12">
 
                             <br>
 
+                            <!-- champs prix -->
                             <div class="bo4 of-hidden size15 m-b-20">
                                 <input class="sizefull s-text7 p-l-22 p-r-22" id="prix" type="number" name="price" placeholder="Prix du produit en CHF" required>
                             </div>
                             <br>
+                            <!-- Drag and drop -->
 <!--
+
                             <h4>
                                 Insertion Images
                             </h4>
@@ -60,18 +84,23 @@ ob_start();
                                 <progress id="progress-bar" max=100 value=0></progress>
                             </div>
                             <script src="view/contents/js/draganddrop.js"></script>-->
+
                             <h4>
                                 Adresse
                             </h4>
 
+                            <!-- champs rue -->
                             <div class="bo4 of-hidden size15 m-b-20">
                                 <br> <input class="sizefull s-text7 p-l-22 p-r-22" id="description" type="text" name="street" placeholder="Rue" required>
                             </div>
 
+                            <!-- champs ville, npa -->
                             <div class="bo4 of-hidden size15 m-b-20" >
                                 <br> <input class="sizefull s-text7 p-l-22 p-r-22" id="description" type="text" name="city" placeholder="Ville, NPA" required>
                             </div>
+
                             <br>
+
                             <div class="boutonmenuad">
                                 <button type="submit" class="button">
                                     Valider mon annonce
